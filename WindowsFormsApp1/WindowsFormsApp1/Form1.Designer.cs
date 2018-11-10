75f2e75bd15c1b10b01a138f864f65a2e2d86741
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2018-11-10 11:58:23 +0700

    giao dien employee

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -56,6 +56,24 @@
             this.tcJob = new System.Windows.Forms.TabPage();
             this.tcEmployee = new System.Windows.Forms.TabPage();
             this.tcAdmin = new System.Windows.Forms.TabPage();
+            this.btnAddJob = new System.Windows.Forms.Button();
+            this.lbUngTuyen = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.btnNxt = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -68,6 +86,13 @@
             this.panel6.SuspendLayout();
             this.tc.SuspendLayout();
             this.tcJob.SuspendLayout();
+            this.tcEmployee.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel12.SuspendLayout();
             this.SuspendLayout();
             // 
             // picBox1
@@ -358,6 +383,12 @@
             // 
             // tcEmployee
             // 
+            this.tcEmployee.Controls.Add(this.panel12);
+            this.tcEmployee.Controls.Add(this.panel9);
+            this.tcEmployee.Controls.Add(this.panel7);
+            this.tcEmployee.Controls.Add(this.lbUngTuyen);
+            this.tcEmployee.Controls.Add(this.btnAddJob);
+            this.tcEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.tcEmployee.Location = new System.Drawing.Point(4, 22);
             this.tcEmployee.Name = "tcEmployee";
             this.tcEmployee.Padding = new System.Windows.Forms.Padding(3);
@@ -375,6 +406,178 @@
             this.tcAdmin.TabIndex = 2;
             this.tcAdmin.Text = "Admin";
             this.tcAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnAddJob
+            // 
+            this.btnAddJob.BackColor = System.Drawing.Color.Red;
+            this.btnAddJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddJob.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAddJob.Location = new System.Drawing.Point(6, 6);
+            this.btnAddJob.Name = "btnAddJob";
+            this.btnAddJob.Size = new System.Drawing.Size(512, 61);
+            this.btnAddJob.TabIndex = 0;
+            this.btnAddJob.Text = "Đăng việc";
+            this.btnAddJob.UseVisualStyleBackColor = false;
+            // 
+            // lbUngTuyen
+            // 
+            this.lbUngTuyen.AutoSize = true;
+            this.lbUngTuyen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUngTuyen.Location = new System.Drawing.Point(149, 79);
+            this.lbUngTuyen.Name = "lbUngTuyen";
+            this.lbUngTuyen.Size = new System.Drawing.Size(221, 25);
+            this.lbUngTuyen.TabIndex = 1;
+            this.lbUngTuyen.Text = "Danh sách ứng viên";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.MistyRose;
+            this.panel7.Controls.Add(this.panel11);
+            this.panel7.Controls.Add(this.panel10);
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Location = new System.Drawing.Point(6, 107);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(512, 297);
+            this.panel7.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(434, 0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 69);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Xem CV";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.IndianRed;
+            this.panel8.Controls.Add(this.label1);
+            this.panel8.Controls.Add(this.btn1);
+            this.panel8.Location = new System.Drawing.Point(0, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(509, 69);
+            this.panel8.TabIndex = 5;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.IndianRed;
+            this.panel9.Controls.Add(this.btn2);
+            this.panel9.Controls.Add(this.label2);
+            this.panel9.Location = new System.Drawing.Point(5, 182);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(509, 69);
+            this.panel9.TabIndex = 6;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.IndianRed;
+            this.panel10.Controls.Add(this.btn3);
+            this.panel10.Controls.Add(this.label3);
+            this.panel10.Location = new System.Drawing.Point(0, 150);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(509, 69);
+            this.panel10.TabIndex = 6;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(435, 0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 69);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "Xem CV";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.IndianRed;
+            this.panel11.Controls.Add(this.btn4);
+            this.panel11.Controls.Add(this.label4);
+            this.panel11.Location = new System.Drawing.Point(0, 225);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(509, 69);
+            this.panel11.TabIndex = 7;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(434, 0);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 69);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "Xem CV";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(434, 0);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 69);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "Xem CV";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(3, 3);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(75, 57);
+            this.btnPre.TabIndex = 7;
+            this.btnPre.Text = "Lùi";
+            this.btnPre.UseVisualStyleBackColor = true;
+            // 
+            // panel12
+            // 
+            this.panel12.Controls.Add(this.btnNxt);
+            this.panel12.Controls.Add(this.btnPre);
+            this.panel12.Location = new System.Drawing.Point(352, 410);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(163, 63);
+            this.panel12.TabIndex = 8;
+            // 
+            // btnNxt
+            // 
+            this.btnNxt.Location = new System.Drawing.Point(85, 3);
+            this.btnNxt.Name = "btnNxt";
+            this.btnNxt.Size = new System.Drawing.Size(75, 57);
+            this.btnNxt.TabIndex = 8;
+            this.btnNxt.Text = "Tiếp";
+            this.btnNxt.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -402,6 +605,18 @@
             this.panel6.ResumeLayout(false);
             this.tc.ResumeLayout(false);
             this.tcJob.ResumeLayout(false);
+            this.tcEmployee.ResumeLayout(false);
+            this.tcEmployee.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
+            this.panel12.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -436,6 +651,24 @@
         private System.Windows.Forms.TabPage tcJob;
         private System.Windows.Forms.TabPage tcEmployee;
         private System.Windows.Forms.TabPage tcAdmin;
+        private System.Windows.Forms.Label lbUngTuyen;
+        private System.Windows.Forms.Button btnAddJob;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Button btnNxt;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
     }
 }
 
